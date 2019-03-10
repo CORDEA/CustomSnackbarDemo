b66dea8e7d124bb62a2f2a86a1cba41bb4e1af2d
Yoshihiro Tanaka <contact@cordea.jp>
2019-03-10 14:06:40 +0900

    display snackbar

--- app/src/main/java/jp/cordea/customsnackbardemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/customsnackbardemo/MainActivity.kt
@@ -1,12 +1,17 @@
 package jp.cordea.customsnackbardemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            CustomSnackbar.make(findViewById(R.id.content)).show()
+        }
     }
 }
